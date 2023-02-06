6e1a293b565d9897183ff0da9150ef59094b82f9
Jan Hryniuk <jan.hryniuk@xebia.com>
2023-02-06 15:41:24 +0100

    feat: Improved voter.

--- app/api/src/Entity/User.php
+++ app/api/src/Entity/User.php
@@ -105,7 +105,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
@@ -187,7 +186,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function eraseCredentials()
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
     }
 }
